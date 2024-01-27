7a90a0c52b4b25d69b6ef93b4c73bf92d73e3925
mohammedasimahmed <asim60999@gmail.com>
2024-01-27 14:02:50 +0530

    service worker registration fixed

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -9,43 +9,44 @@
 // This link also includes instructions on opting out of this behavior.
 
 export default function registerServiceWorker() {
-    if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        const swUrl = `https://testpwa-green.vercel.app/service-worker.js`;
-        navigator.serviceWorker
-          .register(swUrl)
-          .then(registration => {
-            registration.onupdatefound = () => {
-              const installingWorker = registration.installing;
-              installingWorker.onstatechange = () => {
-                if (installingWorker.state === 'installed') {
-                  if (navigator.serviceWorker.controller) {
-                    // At this point, the old content will have been purged and
-                    // the fresh content will have been added to the cache.
-                    // It's the perfect time to display a "New content is
-                    // available; please refresh." message in your web app.
-                    console.log('New content is available; please refresh.');
-                  } else {
-                    // At this point, everything has been precached.
-                    // It's the perfect time to display a
-                    // "Content is cached for offline use." message.
-                    console.log('Content is cached for offline use.');
-                  }
+  // process.env.NODE_ENV === "production" && 
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      const swUrl = `https://testpwa-green.vercel.app/firebase-messaging-sw.js`;
+      navigator.serviceWorker
+        .register(swUrl)
+        .then((registration) => {
+          registration.onupdatefound = () => {
+            const installingWorker = registration.installing;
+            installingWorker.onstatechange = () => {
+              if (installingWorker.state === "installed") {
+                if (navigator.serviceWorker.controller) {
+                  // At this point, the old content will have been purged and
+                  // the fresh content will have been added to the cache.
+                  // It's the perfect time to display a "New content is
+                  // available; please refresh." message in your web app.
+                  console.log("New content is available; please refresh.");
+                } else {
+                  // At this point, everything has been precached.
+                  // It's the perfect time to display a
+                  // "Content is cached for offline use." message.
+                  console.log("Content is cached for offline use.");
                 }
-              };
+              }
             };
-          })
-          .catch(error => {
-            console.error('Error during service worker registration:', error);
-          });
-      });
-    }
+          };
+        })
+        .catch((error) => {
+          console.error("Error during service worker registration:", error);
+        });
+    });
   }
-  
-  export function unregisterServiceWorker() {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.ready.then(registration => {
-        registration.unregister()
-      });
-    }
+}
+
+export function unregisterServiceWorker() {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.ready.then((registration) => {
+      registration.unregister();
+    });
   }
+}
